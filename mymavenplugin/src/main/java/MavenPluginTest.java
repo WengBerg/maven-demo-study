831cd6ecac80c2457db2c5d55376fbcb9ff4f9e3
BergWeng <1182936602@qq.com>
2019-06-22 21:54:14 +0800

    maven 示例添加

--- mymavenplugin/src/main/java/MavenPluginTest.java
+++ mymavenplugin/src/main/java/MavenPluginTest.java
@@ -3,11 +3,28 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import java.util.List;
 
 // name 为goal的名字
 @Mojo(name = "testPlugin",defaultPhase = LifecyclePhase.VERIFY)
 public class MavenPluginTest extends AbstractMojo {
+
+//    外部传入的参数 只能在xml中配置
+    @Parameter
+    private String message;
+    //    外部传入的参数 可在xml中配置，也能通过-D的命令传入
+    @Parameter(property = "test1")
+    private String test1;
+
+    @Parameter
+    private List<String> stringList;
+
     public void execute() throws MojoExecutionException, MojoFailureException {
-        System.out.println("my maven plugin");
+        System.out.println("通过xml传入的参数:"+message);
+        System.out.println("通过命令传入的参数:"+test1);
+        for (String str:stringList) {
+            System.out.println(str);
+        }
     }
 }
